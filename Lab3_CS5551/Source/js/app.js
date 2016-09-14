80b71414290715ff3f622c22ae0641716116fbfb
janziska <jwnf7b@mail.umkc.edu>
2016-09-14 09:59:47 -0500

    fixed sign out

--- Lab3_CS5551/Source/js/app.js
+++ Lab3_CS5551/Source/js/app.js
@@ -15,6 +15,50 @@
 
 
 
+// On page load, initialize Google SSO Info
+function onLoad() {
+    gapi.load('auth2', function() {
+        var auth2 = gapi.auth2.init();
+
+        // If not logged in, redirect to login
+        auth2.then(function () {
+            if (!auth2.isSignedIn.get()) {
+                window.location.href="login.html";
+            }
+        });
+    });
+}
+// Sign out function
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        window.location.href="login.html";
+    });
+
+
+}
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+
+function onSuccess(googleUser) {
+    console.log('Signed in as: ' + googleUser.getBasicProfile().getName());
+    window.location.href="translate.html";
+}
 angular.module('myApp', [])
     .controller('View1Ctrl', function ($scope, $http) {
 
@@ -38,6 +82,8 @@ angular.module('myApp', [])
                       "apikey=6374c076c0afdefeb93b382ecf5610fb71710307&outputMode=json&text=" + transText)
                       .then(function(response2){
                             console.log(transText);
+
+                          // Angular broke Google Sign Out so I had to stop using it
                           $scope.score =  "Score: " + parseFloat(response2.data.docSentiment.score*100).toFixed(0);
                           $scope.type =   "Type:  " +  response2.data.docSentiment.type;
                           // Set width pasted on score
@@ -45,7 +91,7 @@ angular.module('myApp', [])
                           var type = response2.data.docSentiment.type;
 
 
-                         // thanks to w3 School for the idea
+                          // thanks to w3 School for the idea
                           // Create meter
                           document.getElementById("canvas_head").innerHTML = "0 &nbsp &nbsp &nbsp &nbsp &nbsp " +
                               "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" +
