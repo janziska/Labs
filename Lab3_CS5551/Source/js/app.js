8e2f51a02707a70b0a3c3b353f86114257506056
janziska <jwnf7b@mail.umkc.edu>
2016-09-14 09:07:45 -0500

    add code

---
+++ Lab3_CS5551/Source/js/app.js
@@ -0,0 +1,84 @@
+/**
+ * Created by janziska on 9/9/2016.
+ */
+'use strict';
+
+// Uclassify Api key for mood
+// read yyckHiiOgGhQ
+// Write t0gVcFMv7JGd
+
+// Google client id - 872368336329-smi8nn873rvhopil30abkemqn1lukuh9.apps.googleusercontent.com
+// client secret - VJL00eeSj7buen7p6hyuWm1G
+// https://www.googleapis.com/language/translate/v2?key=YOUR_API_KEY&q=hello%20world&source=en&target=de
+// simple google api - AIzaSyCvMpcuLjolygMmSkHIIHIgnkq-10yIEXM
+
+
+
+
+angular.module('myApp', [])
+    .controller('View1Ctrl', function ($scope, $http) {
+
+
+
+        $scope.myFunction = function(){
+
+            var sourceText = document.getElementById("sourceText").value;
+            var sourceLanguage = document.getElementById("from").value;
+            var targetLanguage = document.getElementById("to").value;
+
+           $http.get("https://www.googleapis.com/language/translate/" + "v2?key=AIzaSyCvMpcuLjolygMmSkHIIHIgnkq-10yIEXM&source="
+               + sourceLanguage + "&target=" + targetLanguage + "&q=" + sourceText)
+               .then(function(response){
+
+                   $scope.transText = "Translation: " + response.data.data.translations[0].translatedText;
+                  var transText =  "Translation: " + response.data.data.translations[0].translatedText;
+
+
+                  $http.get("http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?" +
+                      "apikey=6374c076c0afdefeb93b382ecf5610fb71710307&outputMode=json&text=" + transText)
+                      .then(function(response2){
+                            console.log(transText);
+                          $scope.score =  "Score: " + parseFloat(response2.data.docSentiment.score*100).toFixed(0);
+                          $scope.type =   "Type:  " +  response2.data.docSentiment.type;
+                          // Set width pasted on score
+                          var width = response2.data.docSentiment.score*200;
+                          var type = response2.data.docSentiment.type;
+
+
+                         // thanks to w3 School for the idea
+                          // Create meter
+                          document.getElementById("canvas_head").innerHTML = "0 &nbsp &nbsp &nbsp &nbsp &nbsp " +
+                              "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" +
+                              "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 100";
+                          document.getElementById("canvas").style.border = "1px solid #000000";
+                          var c = document.getElementById("canvas");
+                          var ctx = c.getContext("2d");
+
+                          // Create gradient
+                          var grd = ctx.createLinearGradient(0,0,200,0);
+
+                          // Set color for positive or negative
+                          if(type=="positive")
+                          {
+                              grd.addColorStop(0,"green");
+                          }
+                          else
+                          {
+                              grd.addColorStop(0,"red");
+                          }
+
+                          grd.addColorStop(1,"white");
+                            // Fill with gradient
+                          ctx.fillStyle = grd;
+
+                          ctx.fillRect(10,10,width,80);
+
+
+                      });
+
+               });
+
+
+
+            }
+    });
