f80c2485e1ea6fb5ca3161e3e06c4992df80c8b0
janziska <jwnf7b@mail.umkc.edu>
2016-09-26 21:27:00 -0500

    lab5 comments

--- Lab5/Source/myLab5/www/js/controllers.js
+++ Lab5/Source/myLab5/www/js/controllers.js
@@ -2,11 +2,13 @@ angular.module('starter.controllers', [])
 
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state, $window) {
     $scope.data = {};
- 
+    // Login function
     $scope.login = function() {
         LoginService.loginUser($scope.data.username, $scope.data.password, localStorage.getItem('user.name'), localStorage.getItem('user.password')).success(function(data) {
+           // It worked head to the homepage
             $state.go('home');
         }).error(function(data) {
+            // Wrong stuff, try again
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
                 template: 'Please check your credentials!'
@@ -14,51 +16,45 @@ angular.module('starter.controllers', [])
         });
     }  
 
-
+    // Go to the registration page
    $scope.goRegister = function() {
-      
+       
             $state.go('register');
-      
             var alertPopup = $ionicPopup.alert({
                 title: 'Leaving Page',
                 template: 'Going to Register'
             });
-
     }  
-
-
 })
 
-
+// Register a user
 .controller('RegisterCtrl', function($scope, $state) {
 
     $scope.signupEmail = function(name, username, email, password){
   
+    // Make sure they can use local storage before taking the data
     if (typeof(Storage) !== "undefined") {
+
       //Create a new user on Parse
       localStorage.setItem('user.name', name);
       localStorage.setItem('user.username',username);
       localStorage.setItem('user.password', password);
       localStorage.setItem('user.email', email);
   } else {
+    // No local storage :()
     var alertPopup = $ionicPopup.alert({
                   title: 'Error',
                   template: 'No Local Storage on Browser'
               });
   }
-   alert(localStorage.getItem('user.name'));
+        // Go to the login page
       $state.go('login'); 
-     
-    
   }
 
   
 })
 
-
-
-
-
+// Home page lookup
 .controller('HomeCtrl', function($scope, $http, $ionicPopup, $state, $window) {
      $scope.myFunction = function(source){
            $scope.venueList = new Array();
@@ -74,7 +70,7 @@ angular.module('starter.controllers', [])
                    var keyWord = response.data.keywords[0].text;
                     $http.get("https://api.foursquare.com/v2/venues/search?client_id=EMPP4S253KDVUU5KJHWUCOEANVTQJP1O11SOKYMZW2TY2Y2A&client_secret=4ZDMZR2EVRZDAV13JBYLT4WNRPO4GBEDOFXTOZ0UZTH42AFA&v=20160215&limit=5&near=Grandview&query=" + keyWord)
                           .then(function(response2){
-                            
+                            // Borrowed this from Tutorial 3
                             for (var i = 0; i < response2.data.response.venues.length; i++) {
                             $scope.venueList[i] = {
                                 "name": response2.data.response.venues[i].name,
@@ -82,6 +78,7 @@ angular.module('starter.controllers', [])
                                 "location": response2.data.response.venues[i].location.address
                             };
                         }
+                            // this did not work, hopefully I can fix it before the deadline
                           //  $http.get("https://api.foursquare.com/v2/venues/explore/&venuePhotos=1&client_id=EMPP4S253KDVUU5KJHWUCOEANVTQJP1O11SOKYMZW2TY2Y2A&client_secret=4ZDMZR2EVRZDAV13JBYLT4WNRPO4GBEDOFXTOZ0UZTH42AFA&v=20160215&VENUE_ID=" + response2.data.response.venues[0].id)
                          // .then(function(response3){
                            //   console.log(response3.data);
--- Lab5/Source/myLab5/www/js/services.js
+++ Lab5/Source/myLab5/www/js/services.js
@@ -1,5 +1,6 @@
 angular.module('starter.services', [])
 
+// Login for user
 .service('LoginService', function($q) {
     return {
         loginUser: function(name, pw, uname, upass) {
@@ -7,11 +8,14 @@ angular.module('starter.services', [])
             var deferred = $q.defer();
             var promise = deferred.promise;
             
+            // Checks default and registered user in local storage
             if ((name == 'user' && pw == 'secret') || (name == uname && pw == upass)) {
                 deferred.resolve('Welcome ' + name + '!');
             } else {
+                // try again
                 deferred.reject('Wrong credentials.');
             }
+            // Worked
             promise.success = function(fn) {
                 promise.then(fn);
                 return promise;
