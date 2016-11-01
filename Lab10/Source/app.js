fd81a40987bfe54defed4bca0e7b7e5da93b384d
janziska <jwnf7b@mail.umkc.edu>
2016-11-01 14:03:55 -0500

    lab 10 part 2

--- Lab10/Source/app.js
+++ Lab10/Source/app.js
@@ -1,3 +1,5 @@
+
+// Initial info and includes
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
@@ -5,9 +7,11 @@ var port = process.env.PORT || 3000,
     html = fs.readFileSync('index.html');
     register = fs.readFileSync('register.html');
     main = fs.readFileSync('main.html');
+    update = fs.readFileSync('update.html');
 
-
+// Server
 var server = http.createServer(function (req, res) {
+    // Generic post to start task
     if (req.method === 'POST') {
         var body = '';
 
@@ -26,14 +30,17 @@ var server = http.createServer(function (req, res) {
             res.end();
         });
     } 
+// Load pages
 else if (req.method === 'GET')
 {
+    // Main page
     if (req.url === '/main.html') {
                 res.writeHead(200);
                 res.write(main);
                 res.end();
                 console.log('Rendering main.html');
             }
+    // Registration
     else if (req.url === '/register.html') {
                 res.writeHead(200);
                 res.write(register);
@@ -41,7 +48,16 @@ else if (req.method === 'GET')
                 res.end();
                 console.log('Rendering register.html');
             }
-            else {
+    // Update user           
+    else if (req.url === '/update.html') {
+                res.writeHead(200);
+                res.write(update);
+                
+                res.end();
+                console.log('Rendering update.html');
+            }
+     // Load main page if all else fails
+    else {
                 res.writeHead(200);
                 res.write(html);
                 res.end();
--- Lab10/Source/mongo.js
+++ Lab10/Source/mongo.js
@@ -12,14 +12,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Register/insert
 app.post('/register', function (req, res) {
     MongoClient.connect(url, function(err, db) {
+        // Bad connection
         if(err)
         {
+            console.log(req.body);
             res.status(500);
             res.write("Error 1 - Failed, Error while connecting to Database");
             res.end();
         }
+        // See if it is in there first
         db.collection('test').findOne({"email": req.body.email}, function(error, myDoc) {
             if (error) {
                 res.status(500);
@@ -27,12 +31,15 @@ app.post('/register', function (req, res) {
                 console.log("Error 7 - Database access error, please try again");
                 res.end();
             }
+
+            // Already have that, stop the process
             if (myDoc){
                 res.status(400);
                 res.write("Error 8 - Email already registered");
                 console.log("Error 8 - Email already registered");
                 res.end();
             }
+            // Clear to insert
             else
             {
                 db.collection('test').insertOne(req.body, function(err, result) {
@@ -43,6 +50,7 @@ app.post('/register', function (req, res) {
                         console.log("Registration Failed, Error While Registering");
                         res.end();
                     }
+                    res.status(200);
                     console.log("Inserted a document into the restaurants collection.");
                    res.write("Inserted a document into the restaurants collection.");
                     res.end();
@@ -53,7 +61,7 @@ app.post('/register', function (req, res) {
 })
 
 app.post('/login', function (req, res) {
-
+    // Connect to dv
     MongoClient.connect(url, function(err, db) {
         if(err)
         {
@@ -62,22 +70,27 @@ app.post('/login', function (req, res) {
             console.log("Error 2 - Failed, Error while connecting to Database");
             res.end();
         }
-
-
+        
+        // Do we have that password, email combo
         db.collection('test').findOne({"email": req.body.email, "password": req.body.password}, function(error, myDoc) {
+            // Conection issue
             if (error) {
                 res.status(500);
                 res.write("Error 3 - Database access error, please try again");
                 console.log("Error 3 - Database access error, please try again");
                 res.end();
             }
+            // got it, clear to login, send back user data 
             if (myDoc){
+                res.status(200);
                 var result = {'response' : {'email' : myDoc.email, 'fname' : myDoc.fname,  'lname' : myDoc.fname}};
                 res.contentType('application/json');
                 console.log(JSON.stringify(result));
                 res.write(JSON.stringify(result));
                 res.end();
             }
+
+            // Nope, not there
             else
             {
                 res.status(400);
@@ -91,8 +104,102 @@ app.post('/login', function (req, res) {
 
 })
 
+// Delete
+app.post('/delete', function (req, res) {
+    console.log(req.body.email);
+    MongoClient.connect(url, function(err, db) {
+        // Connection error
+        if(err)
+        {
+            res.status(500);
+            res.write("Error 9 - Failed, Error while connecting to Database");
+            console.log("Error 9 - Failed, Error while connecting to Database");
+            res.end();
+        }
+        // Delete it
+        else
+        {
+            db.collection('test').remove({"email": req.body.email}, function(error, result) {
+            // Not there
+            if (error) {
+                res.status(500);
+                res.write("Error 3 - Database access error, please try again");
+                console.log("Error 3 - Database access error, please try again");
+                res.end();
+            }
+            // Deleted
+            else
+            {
+                res.status(200);
+                res.write("Success - " + result);
+                console.log("Success - " + result);
+                res.end();
+            }
+        });
+        }
+       
 
+    });
 
+})
+// Update the user
+app.post('/update', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        // Connection error
+        if(err)
+        {
+            res.status(500);
+            res.write("Error 13 - Failed, Error while connecting to Database");
+            res.end();
+        }
+        // Make sure it is there
+          db.collection('test').findOne({"email": req.body.oldemail}, function(error, myDoc) {
+            // Something went wrong
+            if (error) {
+                res.status(500);
+                res.write("Error 7 - Database access error, please try again");
+                console.log("Error 7 - Database access error, please try again");
+                res.end();
+            }
+            // Found it, make an update
+            if (myDoc){
+               db.collection('test').update({"email": req.body.oldemail}, {$set:{"newemail" : req.body.newemail, "fname" : req.body.fname, "lname" : req.body.lname, "password" : req.body.password}},  function(err, result) {
+                   // Update error
+                    if(err)
+                    {
+                        res.status(400);
+                        res.write("Update Failed, Error While Updating");
+                        console.log("Update Failed, Error While Updating");
+                        res.end();
+                    }
+
+                    // Update worked
+                    else{
+                        res.status(200);
+                        console.log("Updated your info");
+                    res.write("Updated your info");
+                        res.end();
+                    }
+                    
+                });
+            }
+
+            // User does not exist
+            else
+            {
+                res.status(500);
+                res.write("Error 15 - User does not exist");
+                console.log("Error 15 - User does not exist");
+                res.end();
+
+            }
+       
+          });
+     
+    });
+})
+
+// Server port and address
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
