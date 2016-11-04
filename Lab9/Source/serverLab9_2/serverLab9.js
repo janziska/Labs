2b547f221dcb4b3b7717a10fc6b4bf4c09c3301d
janziska <jwnf7b@mail.umkc.edu>
2016-11-04 16:43:03 -0500

    fix

--- Lab9/Source/serverLab9_2/serverLab9.js
+++ Lab9/Source/serverLab9_2/serverLab9.js
@@ -14,8 +14,7 @@ var fs = require('fs');
 // Relative path to image foler
 app.use(express.static(path.join(__dirname, 'img')));
 
-// Backup of image, allows user to retreive last message sent
-var filetest = require('fs').readFileSync('./img/test1.jpg').toString('base64');
+
 
 // Logging to a file
 var log = require('simple-node-logger').createSimpleLogger('./logs/lab9.log');
@@ -30,6 +29,8 @@ app.use(function(req, res, next) {
 
 // Translation of Last Image Sent (for testing purposes)
 app.get('/getTran', function (req, res) {
+  // Backup of image, allows user to retreive last message sent
+var filetest = require('fs').readFileSync('./img/test1.jpg').toString('base64');
     // Results  
     var result;
     var resultSent;
@@ -107,6 +108,7 @@ app.get('/getTran', function (req, res) {
         var translatedText = JSON.parse(body).data.translations[0].translatedText;
         // Send user a response
         result = {'text' : desc, 'translatedText' : translatedText};
+         res.status(200);
         res.contentType('application/json');
         res.write(JSON.stringify(result));
       
@@ -122,10 +124,15 @@ app.get('/getTran', function (req, res) {
 
 // Post an image and get the translation
 app.post('/photo', function(req, res){
+// Backup of image, allows user to retreive last message sent
+
+
 
   // create an incoming form object
   var form = new formidable.IncomingForm();
-
+  var result;
+    var resultSent;
+    var resultType;
   // specify that we want to allow the user to upload multiple files in a single request
   form.multiples = true;
 
@@ -150,9 +157,7 @@ app.post('/photo', function(req, res){
   // once all the files have been uploaded, send a response to the client
   form.on('end', function() {
         // Results  
-    var result;
-    var resultSent;
-    var resultType;
+  
     
     // Read the paramteres for languages
     var source = req.param('source');
@@ -162,7 +167,7 @@ app.post('/photo', function(req, res){
 
     console.log("Source = " + source);  
     console.log("Target = " + target);
-    
+    var filetest = require('fs').readFileSync('./img/test1.jpg').toString('base64');
     // Body to send Image to Google Vision
     var body = {
       requests: {
@@ -233,6 +238,7 @@ app.post('/photo', function(req, res){
          console.log(result);
          
          // Send the response
+         res.status(200);
         res.contentType('application/json');
         res.write(JSON.stringify(result));
        
